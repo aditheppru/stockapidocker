86d5f358bcafa992c0f7060077039e975cefd8a3
Adithep Pruekpitakpong <adithep@buzzebees.com>
2021-06-11 14:05:41 +0700

    first commit

---
+++ StockApi/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Id = "1",
+                Name = "Dell Computer"
+            });
+        }
+    }
+}
